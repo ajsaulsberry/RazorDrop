e7a0f5b473e5442add3c1a8b70adbd462e10f7e8
A. J. Saulsberry <ajsaulsberry@gmail.com>
2020-04-25 16:17:15 -0400

    Added ViewModels folder and view model classes.

---
+++ RazorDrop/ViewModels/CustomerEditViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace RazorDrop.ViewModels
+{
+    public class CustomerEditViewModel
+    {
+        [Display(Name = "Customer Number")]
+        public string CustomerId { get; set; }
+
+        [Required]
+        [Display(Name = "Customer Name")]
+        [StringLength(75)]
+        public string CustomerName { get; set; }
+
+        [Required]
+        [Display(Name = "Country")]
+        public string SelectedCountryId { get; set; }
+        public IEnumerable<SelectListItem> Countries { get; set; }
+
+        [Required]
+        [Display(Name = "State / Region")]
+        public string SelectedRegionId { get; set; }
+        public IEnumerable<SelectListItem> Regions { get; set; }
+    }
+}
