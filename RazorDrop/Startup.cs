1eb3aa56169fc47cd67f7b7cbc27147377cb3406
A. J. Saulsberry <ajsaulsberry@gmail.com>
2020-04-25 16:05:24 -0400

    Added DbContext to Startup.

--- RazorDrop/Startup.cs
+++ RazorDrop/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using RazorDrop.Data;
 
 namespace RazorDrop
 {
@@ -24,6 +26,9 @@ namespace RazorDrop
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+
+            services.AddDbContext<RazorDropContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("RazorDropContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
