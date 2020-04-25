f313d38e7b91428213fc7d58899017b44cb9bbd1
A. J. Saulsberry <ajsaulsberry@gmail.com>
2020-04-25 16:30:09 -0400

    Added Customers folder to Pages and Customers/Index.

---
+++ RazorDrop/Pages/Customers/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorDrop.Data;
+using RazorDrop.ViewModels;
+
+namespace RazorDrop.Pages.Customers
+{
+    public class IndexModel : PageModel
+    {
+
+        private readonly RazorDropContext _context;
+
+        [BindProperty(SupportsGet = true)]
+        public List<CustomerDisplayViewModel> CustomersDisplayList { get; set; }
+
+        public IndexModel(RazorDropContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            var repo = new CustomersRepository(_context);
+            CustomersDisplayList = repo.GetCustomers();
+            return Page();
+        }
+    }
+}
